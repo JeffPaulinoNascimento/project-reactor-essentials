187c0d012a2d8abecd9f0df205a4b5e55ed59302
Jefferson Paulino <jeffersonn@ciandt.com>
2022-10-06 10:51:16 -0300

    first commit test

--- src/test/java/academy/devdojo/reactive/test/MonoTest.java
+++ src/test/java/academy/devdojo/reactive/test/MonoTest.java
@@ -2,13 +2,25 @@ package academy.devdojo.reactive.test;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 @Slf4j
 class MonoTest {
 
     @Test
-    void test() {
-        log.info("Everthing working as intended");
+    void monoSubscriber() {
+        String name = "Jeff";
+        Mono<String> mono = Mono.just(name).log();
+
+        mono.subscribe();
+
+        log.info("--------------------------------");
+
+        StepVerifier.create(mono)
+            .expectNext(name)
+            .verifyComplete();
+
     }
 
 }
