a9d4776c523d62976569027f8432fcc5d1ec7c8c
Jefferson Paulino <jeffersonn@ciandt.com>
2022-10-13 14:08:31 -0300

    add operators test

--- src/test/java/academy/devdojo/reactive/test/OperatorsTest.java
+++ src/test/java/academy/devdojo/reactive/test/OperatorsTest.java
@@ -152,6 +152,32 @@ class OperatorsTest {
     }
 
     @Test
+    void concatOperatorError() {
+
+        Flux<String> flux1 = Flux.just("a", "b")
+            .map(s -> {
+                if (s.equals("b")) {
+                    throw new IllegalArgumentException();
+                }
+                return s;
+            });
+        Flux<String> flux2 = Flux.just("c", "d");
+
+        // para adiar o erro
+        Flux<String> concatFlux = Flux.concatDelayError(flux1, flux2)
+            .log();
+
+        StepVerifier
+            .create(concatFlux)
+            .expectSubscription()
+            .expectNext("a","c", "d")
+            .expectError()
+            .verify();
+
+
+    }
+
+    @Test
     void concatWithOperator() {
         Flux<String> flux1 = Flux.just("a", "b");
         Flux<String> flux2 = Flux.just("c", "d");
@@ -191,10 +217,99 @@ class OperatorsTest {
 
     }
 
-    private Flux<Object> emptyFlux() {
-        return Flux.empty();
+    @Test
+    void mergeOperator() {
+
+        Flux<String> flux1 = Flux.just("a", "b");
+        Flux<String> flux2 = Flux.just("c", "d");
+
+        Flux<String> mergeFlux = Flux.merge(flux1, flux2).log();
+
+        StepVerifier
+            .create(mergeFlux)
+            .expectSubscription()
+            .expectNext("a", "b", "c", "d")
+            .expectComplete()
+            .verify();
+
+    }
+
+    @Test
+    void mergeWithOperator() {
+
+        Flux<String> flux1 = Flux.just("a", "b");
+        Flux<String> flux2 = Flux.just("c", "d");
+
+        Flux<String> mergeFlux = flux1.mergeWith(flux2).log();
+
+        StepVerifier
+            .create(mergeFlux)
+            .expectSubscription()
+            .expectNext("a", "b", "c", "d")
+            .expectComplete()
+            .verify();
+
     }
 
 
+    @Test // faz o merge da string em ordem sequencial mesmo com o delay colocado na variavel flux1
+    void mergeSequentialOperator() {
+        Flux<String> flux1 = Flux.just("a", "b").delayElements(Duration.ofMillis(200));
+        Flux<String> flux2 = Flux.just("c", "d");
+
+        Flux<String> mergeFlux = Flux.mergeSequential(flux1,flux2, flux1)
+            .delayElements(Duration.ofMillis(200))
+            .log();
+
+        StepVerifier
+            .create(mergeFlux)
+            .expectSubscription()
+            .expectNext("a", "b", "c", "d", "a", "b")
+            .expectComplete()
+            .verify();
+
+    }
+
+    @Test
+    void mergeDelayErrorOperator() {
+
+        Flux<String> flux1 = Flux.just("a", "b")
+            .map(s -> {
+                if (s.equals("b")) {
+                    throw new IllegalArgumentException();
+                }
+                return s;
+            })
+            .doOnError(throwable -> log.error("We could do something with this"));
+
+        Flux<String> flux2 = Flux.just("c", "d");
+
+        Flux<String> mergeFlux = Flux.mergeDelayError(1, flux1,flux2, flux1)
+            .log();
+
+        StepVerifier
+            .create(mergeFlux)
+            .expectSubscription()
+            .expectNext("a", "c", "d", "a")
+            .expectError()
+            .verify();
+
+    }
+
+    void flatMapOperator() {
+
+        Flux<String> flux1 = Flux.just("a", "b");
+
+//        flux1.
+
+    }
+
+    public Flux<String> findByName(String name) {
+        return name.equals("A") ? Flux.just("nameA1", "nameA2") : Flux.just("nameB1", "nameB2");
+    }
+
+    private Flux<Object> emptyFlux() {
+        return Flux.empty();
+    }
 
 }
